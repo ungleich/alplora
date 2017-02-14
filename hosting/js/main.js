5d7b4227116e8231a68c7590f963aafd96a1ca36
HenryGBC <henry.bravo89@gmail.com>
2017-02-14 14:26:36 -0430

    Add slide background header

--- hosting/js/main.js
+++ hosting/js/main.js
@@ -1,29 +1,38 @@
- $(window).load(function(){
-    
-    // Page loader
-    
-    $("body").imagesLoaded(function(){
-        $(".page-loader div").fadeOut();
-        $(".page-loader").delay(200).fadeOut("slow");
-    });
-    
-});
+(function($){
+	'use strict';
 
- 
-$( document ).ready(function() {
+	$(window).load(function(){
 
-	$('#confirm-cancel').on('click', '.btn-ok', function(e) {
-		$('#virtual_machine_cancel_form').trigger('submit');
+	// Page loader
+
+		$("body").imagesLoaded(function(){
+		    $(".page-loader div").fadeOut();
+		    $(".page-loader").delay(200).fadeOut("slow");
+		});
+
 	});
 
-  var hash = window.location.hash;
-  hash && $('ul.nav a[href="' + hash + '"]').tab('show');
 
-  $('.nav-tabs a').click(function (e) {
-    $(this).tab('show');
-    var scrollmem = $('body').scrollTop() || $('html').scrollTop();
-    window.location.hash = this.hash;
-    $('html,body').scrollTop(scrollmem);
-  });
+	$( document ).ready(function() {
 
-});
+		initBackgroundsHeader();
+	});
+	
+	//Infinite loop for change Background Header
+	function _changeBg(index, arrayEl, time){
+		if(index===0)
+			$(arrayEl[0]).fadeIn(2000);
+		setTimeout(function(){
+            $(arrayEl[index]).fadeOut(2000);
+            if(index+1 < arrayEl.length)
+            	$(arrayEl[index+1]).fadeIn(2000);
+
+           return index+1 === arrayEl.length ? _changeBg(0, arrayEl, time): _changeBg(index+1, arrayEl, time);
+        }, time);
+	}
+	function initBackgroundsHeader(){
+		var arrayElements = Array.from($('.intro-header .bg-slide'));
+		_changeBg(0, arrayElements, 4000);
+	}
+
+})(jQuery);
